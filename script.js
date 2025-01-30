1a5585c3038280618ef02795e801d7848cbf9249
bilal-marwat <bilalmarwat134@gmail.com>
2025-01-30 12:51:35 +0500

    100 Days Of Day 1 - Alert Box App

---
+++ script.js
@@ -0,0 +1,10 @@
+// select the button 
+const btn = document.querySelector('.btn');
+
+// click event on the button when user clicks the button Message will be show in alert Box
+btn.addEventListener('click', () => {
+    alert('Hello World 🚀 Day 1 - My 100 Days of JavaScript Challenge Begins!');
+    
+
+
+});
